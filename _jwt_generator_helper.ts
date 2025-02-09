71e03bf300bb450afe3ac9f1b71fa5939f11ee09
Leonardo Teixeira Cândido <leonardo.leotc@gmail.com>
2025-02-09 20:28:15 -0300

    fix: production start point

--- _jwt_generator_helper.ts
+++ _jwt_generator_helper.ts
@@ -2,8 +2,8 @@ import * as jwt from 'jsonwebtoken';
 
 // Payload with tenantid and branchid
 const payload = {
-  tenantid: '0e0e19b8-7a60-40f9-bac5-234e84d0bda1',
-  branchid: '116103c8-c380-4b13-9ee7-a0c4bb2b7c7f',
+  tenantid: '08f2a935-eeed-4660-8b3d-b989c06b9ecd',
+  branchid: '3c07abd9-9041-4ed0-b556-0c3a9ac2865c',
 };
 
 // Secret key (Use environment variable in production)
@@ -20,3 +20,13 @@ const options: jwt.SignOptions = {
 const token = jwt.sign(payload, secretKey, options);
 
 console.log('Generated JWT Token:', token);
+
+/**
+ * token 1
+ * Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRpZCI6IjBlMGUxOWI4LTdhNjAtNDBmOS1iYWM1LTIzNGU4NGQwYmRhMSIsImJyYW5jaGlkIjoiMTE2MTAzYzgtYzM4MC00YjEzLTllZTctYTBjNGJiMmI3YzdmIiwiaWF0IjoxNzM4NDEzNTQxLCJleHAiOjE3NDAyMjc5NDF9.a66tu0NFYfFI7ho0OkPa5cXhb7O7qXMIfe092Nk6Hgo
+ */
+
+/**
+ * token 2
+ * Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRpZCI6IjA4ZjJhOTM1LWVlZWQtNDY2MC04YjNkLWI5ODljMDZiOWVjZCIsImJyYW5jaGlkIjoiM2MwN2FiZDktOTA0MS00ZWQwLWI1NTYtMGMzYTlhYzI4NjVjIiwiaWF0IjoxNzM5MTQzMTI0LCJleHAiOjE3NDA5NTc1MjR9.eQVCcSgnPwly5s0TxsJZT-h-YOHCkJ86bn8ULVsHaQM
+ */
